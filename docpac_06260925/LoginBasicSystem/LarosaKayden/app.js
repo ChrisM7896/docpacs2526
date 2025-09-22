c8a2d6daf3559282e4e98110b7b349737b07ff22
Kayden <27KlaRosa@live.ytech.edu>
2025-09-22 11:23:39 -0400

    i done with this ****

--- docpac_06260925/LoginBasicSystem/LarosaKayden/app.js
+++ docpac_06260925/LoginBasicSystem/LarosaKayden/app.js
@@ -2,6 +2,38 @@
 const express = require("express");
 const app = express()
 const sqlite3 = require('sqlite3').verbose();
+const crypto = require('crypto');
 const path = require("path");
 const port = 3000
+const MasterPassword = "ethan likes big black men, source: trust"
 
+var db = new sqlite3.Database("./data/database.db");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+//app gets
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home page' });
+});
+
+app.get('/home', (req, res) => {
+  res.render('home', { title: username });
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup', { title: 'Sign Up page' });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', { title: 'Login Page' });
+});
+
+app.post('/login', (req, res) => {
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
