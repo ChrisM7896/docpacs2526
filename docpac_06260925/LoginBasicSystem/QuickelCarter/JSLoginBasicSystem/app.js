34dd931236f82204ca3053a00363d6875ee668ae
CarterQuickel <27CQuickel@live.ytech.edu>
2025-09-24 07:02:19 -0700

    redoplz

--- docpac_06260925/LoginBasicSystem/QuickelCarter/JSLoginBasicSystem/app.js
+++ docpac_06260925/LoginBasicSystem/QuickelCarter/JSLoginBasicSystem/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const SECRET_KEY = 'a-long-random-string';
+const crypto = require('crypto');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/app.db', (err) => {
@@ -30,10 +31,19 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
+
+// login page form submission
 app.post('/login', (req, res) => {
+    try {
     const { username, password } = req.body;
     res.locals.user = req.body.username;
-    db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
+    crypto.scrypt(password, SECRET_KEY, 32, (err, derivedKey) => {
+        if (err) {
+            console.error('Password hashing error:', err.message);
+            res.redirect('/error');
+        }
+    const hashedPassword = derivedKey.toString('hex');
+    db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, hashedPassword], (err, row) => {
         if (err) {
             console.error('Database query error:', err.message);
             res.redirect('/error');
@@ -41,7 +51,12 @@ app.post('/login', (req, res) => {
         if (row) {
             res.render('home', { user: res.locals.user, email: row.email });
         }
+        });
     });
+} catch (error) { 
+    console.error('Unexpected error:', error.message);
+    res.redirect('/error');
+}
 });
 
 //sign up page
@@ -49,24 +64,34 @@ app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+// home page after login/signup
 app.get('/home', (req, res) => {
     res.render('home', { user: res.locals.user, email: res.locals.email });
 });
 
+// handle sign up form submission  
 app.post('/signup', (req, res) => {
     const { username, email, password } = req.body;
     res.locals.user = req.body.username;
     res.locals.email = req.body.email;
-    db.run('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password], function(err) {
+    crypto.scrypt(password, SECRET_KEY, 32, (err, derivedKey) => {
         if (err) {
-            console.error('Database insertion error:', err.message);
+            console.error('Password hashing error:', err.message);
             res.redirect('/error');
-        } else {
-            res.render('home', { user: res.locals.user, email: res.locals.email } );
         }
+        const hashedPassword = derivedKey.toString('hex');
+        db.run('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, hashedPassword], function(err) {
+            if (err) {
+                console.error('Database insertion error:', err.message);
+                res.redirect('/error');
+            } else {
+                res.render('home', { user: res.locals.user, email: res.locals.email } );
+            }
+        });
     });
 });
 
+//error page
 app.get('/error', (req, res) => {
     res.render('error');
 });
