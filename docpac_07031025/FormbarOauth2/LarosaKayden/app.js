2177abf00762196aea15e4b6eb0f217a78234d55
Kayden <27KlaRosa@live.ytech.edu>
2025-10-01 14:55:53 -0400

    bri

--- docpac_07031025/FormbarOauth2/LarosaKayden/app.js
+++ docpac_07031025/FormbarOauth2/LarosaKayden/app.js
@@ -9,14 +9,14 @@ const fs = require("fs");
 const app = express()
 const PORT = 3000;
 
-const AUTH_URL = "https://formbeta.yorktechapps.com";
+const AUTH_URL = "https://formbeta.yorktechapps.com/";
 const THIS_URL = "http://localhost:3000/login";
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let db = new sqlite3.Database("./data/database.db", (err) => {
+const db = new sqlite3.Database("./data/database.db", (err) => {
     if (err) {
         console.error(err.message);
     }
@@ -30,34 +30,59 @@ app.use(session({
 }));
 
 function isAuthenticated(req, res, next) {
-    console.log("Checking Auth")
     if (req.session.user) next()
-    else res.redirect(`/login?redirectURL=${THIS_URL}`)
+    else res.redirect(`/login`)
 }
 
 app.get('/', isAuthenticated, (req, res) => {
-    console.log("Root")
     res.render('index')
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', isAuthenticated, (req, res) => {
     console.log(req.query.token)
     if (req.query.token) {
         let tokenData = jwt.decode(req.query.token)
         req.session.token = tokenData
         req.session.user = tokenData.displayName
+        req.session.userid = tokenData.id
         res.redirect('/')
+        db.get('SELECT * FROM users WHERE fb_id = ?', [tokenData.id], (err, row) => {
+            if (err) {
+                console.error(err.message);
+            }
+            if (!row) {
+                db.run('INSERT INTO users (fb_id, name, profile_checked) VALUES (?, ?, ?)', [tokenData.id, tokenData.displayName, 0], (err) => {
+                    if (err) {
+                        console.error(err.message);
+                    }
+                    console.log(`A row has been inserted with fb_id ${tokenData.id}`);
+                });
+            } else {
+                console.log("User already exists")
+            }
+        });
+
     } else {
-        res.redirect(`${AUTH_URL}/oauth?redirectURL=${THIS_URL}`)
+        res.redirect(`${AUTH_URL}oauth?redirectURL=${THIS_URL}`)
     }
 })
-app.get('/profile', (req, res) => {
+app.get('/profile', isAuthenticated, (req, res) => {
+    try {
+        res.render('profile', { user: req.session.user })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+app.post('/profile', (req, res) => {
     if (req.session.user) {
         db.run('UPDATE users SET profile_checked=? WHERE fb_id=?', [req.body.profile_checked, req.session.userid], (err) => {
             if (err) {
                 console.error(err.message);
                 res.status(500).send("Internal Server Error");
             }
+            res.status(200).send("Profile updated.");
         });
     }
 })
