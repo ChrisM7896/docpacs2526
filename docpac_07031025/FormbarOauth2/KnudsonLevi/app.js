7f15b9524444c68456856de37f05c80304ab57b5
Levi Knudson <164372586+27lknudson@users.noreply.github.com>
2025-10-01 14:55:23 -0400

    e

--- docpac_07031025/FormbarOauth2/KnudsonLevi/app.js
+++ docpac_07031025/FormbarOauth2/KnudsonLevi/app.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const session = require('express-session');
-const app = require('express');
+const express = require('express');
+app = express();
 
 const AUTH_URL = 'https://formbeta.yorktechapps.com'
 const THIS_URL = 'http://localhost:3000/login'
@@ -12,3 +13,35 @@ const sessionOptions = {
 }
 
 app.use(session(sessionOptions));
+app.use(path.join(__dirname + 'public'));
+app.set('view engine', 'ejs');
+
+function isAuthenticated(req, res, next) {
+    if (req.session.user) next();
+    else res.redirect('/login');
+}
+
+app.get('/' , isAuthenticated, (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/profile', isAuthenticated, (req, res) => {   
+    res.render('profile.ejs', {user: req.session.user});
+});
+
+app.post('/profile', isAuthenticated, (req, res) => {   
+    boxChecked = req.body.checkbox;
+});
+
+app.get('/login', (req, res) => {
+    if (req.query.token) {
+        let tokenData = jwt.decode(req.query.token);
+        req.session.token = tokenData;
+        req.session.user = tokenData.displayName;
+        res.redirect('/profile');
+    } else {
+        res.redirect(`${AUTH_URL}/oauth?redirectURL=${THIS_URL}`);
+    }
+});
+
+app.listen(3000, () => {console.log('Listening on port 3000');});
