1e9de6d0d2a3aef878cae2ba3a7c7f9dfbac3103
DylanMechler <27DMechler@live.ytech.edu>
2025-10-03 13:30:46 -0400

    MechlerDylan

--- docpac_07031025/FormbarOauth2/MechlerDylan/app.js
+++ docpac_07031025/FormbarOauth2/MechlerDylan/app.js
@@ -10,7 +10,7 @@ const THIS_URL = 'http://localhost:3000/login'
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const db = new sqlite3.Database('./data/templatedatabase.db', (err) => {
+const db = new sqlite3.Database('./data/user.db', (err) => {
     if (err) {
         console.error(err.message);
     }
@@ -27,7 +27,7 @@ app.use(session({
 
 function isAuthenticated(req, res, next) {
     if (req.session.user) next()
-    else res.redirect('/login')
+    else res.redirect(`/login?redirectURL=${THIS_URL}`);
 };
 
 app.get('/', isAuthenticated, (req, res) => {
@@ -39,37 +39,47 @@ app.get('/', isAuthenticated, (req, res) => {
     }
 });
 
-function isAuthenticated(req, res, next) {
-    if (req.session.user) {
-        const tokenData = req.session.token;
-        try {
-            const currentTime = Math.floor(Date.now() / 1000);
-            if (tokenData.exp < currentTime) {
-                throw new Error('Token has expired');
-            }
-            next();
-        } catch (err) {
-            res.redirect(`${AUTH_URL}/oauth?refreshToken=${tokenData.refreshToken}&redirectURL=${THIS_URL}`);
-        }
-    } else {
-        res.redirect(`/login?redirectURL=${THIS_URL}`)
-    }
-}
-
 app.get('/login', (req, res) => {
     if (req.query.token) {
-        let tokenData = jwt.decode(req.query.token);
-        req.session.token = tokenData;
-        req.session.user = tokenData.displayName;
-        res.redirect('/');
-    } else {
-        res.redirect(`${AUTH_URL}?redirectURL=${THIS_URL}`);
-    };
+		let tokenData = jwt.decode(req.query.token)
+		req.session.token = tokenData
+		req.session.user = tokenData.displayName
+		res.redirect('/')
+	} else {
+		res.redirect(`${AUTH_URL}/oauth?redirectURL=${THIS_URL}`)
+	}
 });
 
 app.get('/profile', isAuthenticated, (req, res) => {
-    res.render('profile.ejs', {user : req.session.user})
-})
+    var name = req.session.user
+    db.get(`SELECT * FROM users WHERE fb_name = ?`, [name], (err, row) => {
+        if (row) {
+            res.render('profile.ejs', {user : req.session.user, check: JSON.stringify(row.profile_checked)})
+        } else {
+            db.run(`INSERT INTO users(fb_name, profile_checked) VALUES(?, ?)`, [name, null], function(err) {
+                if (err) {
+                    return console.error(err.message)
+                }
+                console.log(`Row(s) updated: ${this.changes}`)
+                res.render('profile.ejs', {user : req.session.user, check: JSON.stringify(0)})
+            })
+        }
+    })
+});
+
+app.post('/profile', (req, res) => {
+    var name = req.session.user
+    console.log(name)
+    var check = req.body.userCheck
+    console.log(check)
+    db.run('UPDATE users SET profile_checked = ? WHERE fb_name = ?', [check, name], function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`Row(s) updated: ${this.changes}`);
+            res.redirect('/profile');
+        });
+});
 
 app.listen(3000, () => {
     console.log("Started HTTP Server on port 3000");
