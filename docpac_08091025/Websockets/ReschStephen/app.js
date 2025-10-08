c734787177bb613121a321d5de212fbeefcc8ebf
StephenResch <27Sresch@live.ytech.edu>
2025-10-08 11:31:25 -0400

    Checkpoint 2

--- docpac_08091025/Websockets/ReschStephen/app.js
+++ docpac_08091025/Websockets/ReschStephen/app.js
@@ -1,16 +1,25 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+
 const jwt = require('jsonwebtoken');
 const path = require('path');
-const { Server } = require('socket.io');
 const session = require('express-session');
-const express = require('express');
-const app = express();
 const port = 3000;
 const sqlite3 = require('sqlite3').verbose();
-const AUTH_URL = 'https://formbeta.yorktechapps.com'
-const THIS_URL = 'http://localhost:3000/login'
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const AUTH_URL = 'https://formbeta.yorktechapps.com'
+const THIS_URL = 'http://localhost:3000/login'
 
 const db = new sqlite3.Database('./data/database.db', (err) => {
     if (err) {
@@ -20,15 +29,19 @@ const db = new sqlite3.Database('./data/database.db', (err) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
-app.use(session({
+const sessionMiddleware = session({
     secret: 'funny string right here',
     resave: false,
     saveUninitialized: true
-}));
+});
+
+var activeUsers = [];
+
+app.use(sessionMiddleware);
+
+io.use((socket, next) => {
+    sessionMiddleware(socket.request, {}, next);
+});
 
 function isAuthenticated(req, res, next) {
     if (req.session.user) {
@@ -65,4 +78,34 @@ app.get('/chat', isAuthenticated, (req, res) => {
     } catch (error) {
         res.send(error.message);
     }
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    const session = socket.request.session;
+    socket.username = session.user || 'Anonymous';
+    activeUsers.push({username: socket.username, id: socket.id});
+    console.log('Active users:', activeUsers);
+    io.emit('active users', activeUsers);
+    socket.on('chat message', (msg) => {
+        console.log('data:', msg, socket.username);
+        
+        io.emit('chat message', {
+            username: socket.username,
+            message: msg
+        });
+    });
+    socket.on('create room', (roomName) => {
+        io.emit('room created', roomName);
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        activeUsers.pop(activeUsers.findIndex(u => u.id === socket.id));
+        console.log('Active users:', activeUsers);
+        io.emit('active users', activeUsers);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
