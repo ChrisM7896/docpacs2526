62db3625c7d6e5fca0d9011ae3e6e4dd514d9cde
DylanMechler <27DMechler@live.ytech.edu>
2025-10-08 11:31:38 -0400

    MechlerDylan

--- docpac_08091025/Websockets/MechlerDylan/app.js
+++ docpac_08091025/Websockets/MechlerDylan/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
-const socket = require("socket.io")
-const join = require("node:path")
+const { createServer } = require('node:http')
+const { join } = require("node:path")
+const { Server } = require("socket.io")
 //add encryption if necessary
 const app = express();
 const AUTH_URL = 'https://formbeta.yorktechapps.com'
 const THIS_URL = 'http://localhost:3000/login'
+const server = createServer(app);
+const io = new Server(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,11 +23,17 @@ const db = new sqlite3.Database('./data/templatedatabase.db', (err) => {
 
 app.set('view engine', 'ejs');
 
-app.use(session({
+const middleWare = session({
     secret: 'secretString',
     resave: false,
     saveUninitialized: false
-}));
+})
+
+app.use(middleWare);
+
+io.use((socket, next) => {
+    middleWare(socket.request, {}, next)
+})
 
 function isAuthenticated(req, res, next) {
     if (req.session.user) next()
@@ -33,7 +42,7 @@ function isAuthenticated(req, res, next) {
 
 app.get('/', isAuthenticated, (req, res) => {
     try {
-        res.render('chat.ejs', {user : req.session.user})
+        res.redirect('http://localhost:3000/chat')
     }
     catch (error) {
         res.send(error.message)
@@ -42,19 +51,33 @@ app.get('/', isAuthenticated, (req, res) => {
 
 app.get('/login', (req, res) => {
     if (req.query.token) {
-		let tokenData = jwt.decode(req.query.token)
-		req.session.token = tokenData
-		req.session.user = tokenData.displayName
-		res.redirect('/')
-	} else {
-		res.redirect(`${AUTH_URL}/oauth?redirectURL=${THIS_URL}`)
-	}
+        let tokenData = jwt.decode(req.query.token)
+        req.session.token = tokenData
+        req.session.user = tokenData.displayName
+        res.redirect('/')
+    } else {
+        res.redirect(`${AUTH_URL}/oauth?redirectURL=${THIS_URL}`)
+    }
 });
 
 app.get('/chat', isAuthenticated, (req, res) => {
-    res.render('chat.ejs', {user : req.session.user})
+    res.render('chat.ejs', { user: req.session.user })
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    var data = socket.request.session;
+    const user = data.user
+    console.log("User Connected: ", user);
+    io.emit('user connected', user)
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg, user) //add previous messages to new user
+    })
+    socket.on('disconnect', () => {
+        console.log("User Disconnected: ", user)
+        //remove user from list
+    })
+})
+
+server.listen(3000, () => {
     console.log("Started HTTP Server on port 3000");
 });
