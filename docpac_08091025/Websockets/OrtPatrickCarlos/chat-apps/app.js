9806bce4e257c16ebd327f0c04733dbb62d6c902
Carlos Ort-Patrick <27COrtPatrick@live.ytech.edu>
2025-10-09 14:23:17 -0400

    up2

--- docpac_08091025/Websockets/OrtPatrickCarlos/chat-apps/app.js
+++ docpac_08091025/Websockets/OrtPatrickCarlos/chat-apps/app.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const app = express();
 const session = require('express-session');
 const jwt = require('jsonwebtoken');
+const path = require('path');
+
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const AUTH_URL = 'https://formbeta.yorktechapps.com';
 const THIS_URL = 'http://localhost:3000/handle';
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
     session({
@@ -16,6 +24,16 @@ app.use(
         saveUninitialized: true,
     })
 );
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
 function isAuthenticated(req, res, next) {
     if (req.session.user) {
@@ -55,6 +73,11 @@ app.get('/chat', isAuthenticated, (req, res) => {
     res.render('chat', { user: req.session.user });
 });    
 
-app.listen(3000, () => {
+app.post('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
