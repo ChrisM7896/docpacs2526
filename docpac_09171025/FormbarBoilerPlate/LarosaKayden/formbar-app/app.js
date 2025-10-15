e419d8c4b5bb558dbc069fc917def092e164d3e6
27klarosa <27KlaRosa@live.ytech.edu>
2025-10-14 20:00:56 -0400

    im gonna die this asignment kinda broken

--- docpac_09171025/FormbarBoilerPlate/LarosaKayden/formbar-app/app.js
+++ docpac_09171025/FormbarBoilerPlate/LarosaKayden/formbar-app/app.js
@@ -2,10 +2,44 @@
 const express = require('express');
 const app = express();
 
+//requirements
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
+//const fetch = require('node-fetch')
 
-app.get('/', (req, res) => {
-    res.send('helo');
+//view set
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+//session
+const session = require('express-session')
+const SQLiteStore = require('connect-sqlite3')(session);
+
+app.use(session({
+    store: new SQLiteStore({ db: 'sessions.db', dir: './db' }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+//login function
+function isAuthenticated(req, res, next) {
+    if (req.session.user) return next();
+    res.redirect(`/login?redirectURL=${process.env.THIS_URL}`);
+}
+
+//gets
+app.get('/', isAuthenticated, (req, res) => res.render('index', { user: req.session.user }));
+
+app.get('/login', (req, res) => {
+    if (req.query.token) {
+        const data = jwt.decode(req.query.token);
+        req.session.user = data.displayName;
+        res.redirect('/');
+    } else {
+        res.redirect('${process.env.AUTH_URL}/oauth?redirectURL=${process.env.THIS_URL}');
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => {
--- docpac_09171025/FormbarBoilerPlate/LarosaKayden/formbar-app/script/init-db.js
+++ docpac_09171025/FormbarBoilerPlate/LarosaKayden/formbar-app/script/init-db.js
@@ -1,14 +1,29 @@
 import fs from 'fs';
 import sqlite3 from 'sqlite3';
 
-const sql = fs.readFileSync('db/init.sql', 'utf8');
-const db = new sqlite3.Database('db/app.db');
+try {
+    const sql = fs.readFileSync('db/init.sql', 'utf8');
+    const db = new sqlite3.Database('db/app.db', (err) => {
+        if (err) {
+            console.error("database no load", err.message);
+            return;
+        }
+    });
 
-db.exec(sql, (err) => {
-    if (err) {
-        console.error('Error initializing database:', err.message);
-    } else {
-        console.log('Database initialized successfully.');
-    }
-    db.close();
-});
+    db.exec(sql, (err) => {
+        if (err) {
+            console.error('Error running database:', err.message);
+        } else {
+            console.log('Database connect successful.');
+        }
+        db.close((err) => {
+            if (err) {
+                console.error('could not close database:', err.message)
+            } else {
+                console.log('Database close succesful.')
+            }
+        });
+    });
+} catch (err) {
+    console.log("uh-oh, something really really bad happened:", err.message)
+}
