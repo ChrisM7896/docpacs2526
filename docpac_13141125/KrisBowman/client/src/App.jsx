868c04ed58f07b4a372a78788734d99bc360a5e5
Kris Bowman <129105342+kris26658@users.noreply.github.com>
2025-11-12 14:55:31 -0500

    Formbar Oauth
    I figured out how to properly reroute the user to formbar's oauth, and to get their username in my react client. Big progress!

--- docpac_13141125/KrisBowman/client/src/App.jsx
+++ docpac_13141125/KrisBowman/client/src/App.jsx
@@ -12,11 +12,27 @@ function App() {
     }
   };
   
+  function getUsername() {
+    fetch('http://localhost:3001/api/user', {
+      method: 'GET',
+      credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(data => {
+      let userDisplay = document.getElementById('userDisplay');
+      userDisplay.textContent = (`Welcome ${data.username}`);
+    })
+    .catch(error => {
+      console.error('Error fetching username:', error);
+    });
+  }
 
   return (
     <>
       <div>
+      <div id="userDisplay"></div>
         <button onClick={login}>Formbar Oauth</button>
+        <button onClick={getUsername}>Get Username</button>
       </div>
     </>
   )
