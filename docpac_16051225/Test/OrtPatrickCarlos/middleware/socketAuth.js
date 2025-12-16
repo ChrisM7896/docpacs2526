efb8990538eabec388de59ddbc4db9e96cdf840f
Carlos Ort-Patrick <27COrtPatrick@live.ytech.edu>
2025-12-16 10:26:44 -0500

    Removed fake

--- docpac_16051225/Test/OrtPatrickCarlos/middleware/socketAuth.js
+++
@@ -1,7 +0,0 @@
-module.exports = function socketAuth(socket, next) {
-  const session = socket.request && socket.request.session;
-  if (session && session.user) return next();
-  const err = new Error('Unauthorized');
-  err.data = { message: 'User not authenticated' };
-  return next(err);
-};
--- docpac_16051225/Test/OrtPatrickCarlos/sockets/onConnect.js
+++
@@ -1,3 +0,0 @@
-module.exports = function onConnect(socket, io, { logger }) {
-  socket.emit('welcome', { message: 'Welcome to the Socket.IO demo' });
-};
